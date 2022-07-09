01ca191428d102ab95a97dee0bc9dbb95c3a06ce
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2022-07-09 22:26:03 +0300

    python -m json.tool and jq for better output

--- python_json.sh
+++ python_json.sh
@@ -24,3 +24,28 @@ cat test.json | python -m json.tool
 #     "title": "Person",
 #     "type": "object"
 # }
+
+
+#need to install before
+cat test.json | jq
+# {
+#   "title": "Person",
+#   "type": "object",
+#   "properties": {
+#     "firstName": {
+#       "type": "string"
+#     },
+#     "lastName": {
+#       "type": "string"
+#     },
+#     "age": {
+#       "description": "Age in years",
+#       "type": "integer",
+#       "minimum": 0
+#     }
+#   },
+#   "required": [
+#     "firstName",
+#     "lastName"
+#   ]
+# }
